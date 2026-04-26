2af9765e5a46e2629e5aa550ec7518d55bac13b6
basaknilabja <basaknilabja@gmail.com>
2026-04-26 17:12:58 +0530

    Updated db.py neon.db path DATABASE_URL added

--- app/db.py
+++ app/db.py
@@ -3,9 +3,9 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 from dotenv import load_dotenv
+DATABASE_URL="postgresql://neondb_owner:npg_UqgwEu36Fzrf@ep-young-heart-amdkqvlx-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 load_dotenv()  # 👈 THIS IS THE KEY LINE
-
 
 def get_db_connection():
     database_url = os.getenv("DATABASE_URL")
